5c6ad268b031306fa00bfc52b52416ffc24796d1
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-03-25 10:00:39 +0100

    ecriture fonction

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,11 +1,25 @@
-const assert = require('assert');
+const assert = require("assert");
 
 //Fonction
 const capitalizeFirstLetters = (sentence) => {
-
-}
+  console.log(sentence);
+  sentence.trim();
+  if (sentence.length > 0) {
+    return sentence
+      .split(" ")
+      .map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+      })
+      .join(" ");
+  } else {
+    return "";
+  }
+};
 
 //écriture des tests
-assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I am Lerning TDD');
-assert.strictEqual(capitalizeFirstLetters('javascript'), 'Javascript');
-assert.strictEqual(capitalizeFirstLetters(''), '');
+assert.strictEqual(capitalizeFirstLetters(""), "");
+assert.strictEqual(
+  capitalizeFirstLetters("i am learning TDD"),
+  "I Am Learning TDD"
+);
+assert.strictEqual(capitalizeFirstLetters("javascript"), "Javascript");
