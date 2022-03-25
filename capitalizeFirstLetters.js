513f30774582e28894badcb2d9ffa9a753d12200
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-03-25 08:57:46 +0100

    writing tests

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+//Fonction
+const capitalizeFirstLetters = (sentence) => {
+
+}
+
+//écriture des tests
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I am Lerning TDD');
+assert.strictEqual(capitalizeFirstLetters('javascript'), 'Javascript');
+assert.strictEqual(capitalizeFirstLetters(''), '');
